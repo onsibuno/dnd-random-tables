fa1e7dd2ca0096ab0e9d6dd2febc2803f23c6929
MathisAdrar <mathisb.buisson@gmail.com>
2024-10-18 16:08:45 +0200

    debut css et première page

---
+++ src/Controller/dndTablesController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class dndTablesController extends AbstractController
+{
+    #[Route('/tables', name: 'app_dnd_tables')]
+    public function index(): Response
+    {
+
+        $inclinaison = rand(-0.002, 0.002);
+        $numeroPage = rand(1, 4);
+        $lienPage = 'images/pages' . $numeroPage . '.png';
+        
+        return $this->render('dnd_tables/index.html.twig', [
+            'controller_name' => 'DndTablesController',
+            // 'lienBackgroundPage' => $lienPage,
+            // 'styleTilt' => $inclinaison,
+            'lienPage' => $lienPage,
+            'inclinaison' => $inclinaison
+        ]);
+    }
+}
