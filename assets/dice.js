56d0c52abd798c94c31a264c5bac7941dcb45e43
onsibuno <mathisb.buisson@gmail.com>
2024-12-03 22:29:28 +0100

    displayed images with encore, progress on the dice script

--- assets/dice.js
+++ assets/dice.js
@@ -5,21 +5,58 @@ let chosenDice = document.querySelector('#chosenDice').innerHTML;
 
 console.log(chosenDice);
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+};
+
 function rollClick() {
     let dice = 1;
-    if (document.querySelector('#chosenDice').innerHTML === 'd12') {
-        dice = 12;
-    } else if (document.querySelector('#chosenDice').innerHTML === 'd10') {
+
+    if (document.querySelector('#chosenDice').innerHTML === 'd10') {
         dice = 10;
+    } else if (document.querySelector('#chosenDice').innerHTML === '2d10/4') {
+        dice = 25;
+    } else if (document.querySelector('#chosenDice').innerHTML === '2d10/2') {
+        dice = 50;
+    } else if (document.querySelector('#chosenDice').innerHTML === '2d10') {
+        dice = 100;
+    } else if (document.querySelector('#chosenDice').innerHTML === 'd20') {
+        dice = 20;
+    } else if (document.querySelector('#chosenDice').innerHTML === 'd12') {
+        dice = 12;
+    } else if (document.querySelector('#chosenDice').innerHTML === 'd8') {
+        dice = 8;
+    } else if (document.querySelector('#chosenDice').innerHTML === 'd6') {
+        dice = 6;
+    } else if (document.querySelector('#chosenDice').innerHTML === 'd4') {
+        dice = 4;
     }
 
-    console.log('Le résultat du lancé du dé est :' + dice);
-}
+    let result = getRandomInt(dice) + 1
+    console.log('Le résultat du lancé du dé est :' + result);
+    choiceSelection(result);
+};
 
 function diceTextInput() {
+
+};
+
+function choiceSelection(tableRow) {
+    if(document.querySelector(".selectedRow") === null){
+        console.log("if")
+        let selectedRow = document.querySelector('#ligne' + tableRow);
+        selectedRow.className = "selectedRow";
+    } else {
+        console.log("else")
+
+        let previousRow = document.querySelector(".selectedRow");
+        previousRow.classList.remove('selectedRow');
+        let selectedRow = document.querySelector('#ligne' + tableRow);
+        selectedRow.className = "selectedRow";
+    }
 
 }
 
 if (rollButton != null) {
     rollButton.addEventListener('click', rollClick); // On passe notre fonction en référence, c’est pourquoi c’est sans parenthèses
-}
+};
--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,20 @@ Encore
     // only needed for CDN's or subdirectory deploy
     //.setManifestKeyPrefix('build/')
 
+
+    .copyFiles({
+        from: './assets/images',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
+
     /*
      * ENTRY CONFIG
      *
@@ -29,7 +43,7 @@ Encore
     .addEntry('newtable', './assets/styles/newtable/newtable.css')
     .addEntry('register', './assets/styles/register/register.css')
     .addEntry('tables', './assets/styles/tables/tables.css')
-    
+
 
     // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
     .splitEntryChunks()
@@ -80,6 +94,6 @@ Encore
 
     // uncomment if you're having problems with a jQuery plugin
     //.autoProvidejQuery()
-;
+    ;
 
 module.exports = Encore.getWebpackConfig();
