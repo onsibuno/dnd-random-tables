6451e338324545c463bb4dfc967195d0d680ecaa
onsibuno <mathisb.buisson@gmail.com>
2024-12-04 12:02:03 +0100

    finished and cleaned dice script and text input scripts, modified table css

--- assets/dice.js
+++ assets/dice.js
@@ -1,62 +1,64 @@
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
 
 let rollButton = document.querySelector('#diceRoll');
+let textButton = document.querySelector('#textButton');
 let chosenDice = document.querySelector('#chosenDice').innerHTML;
+let tableDice = document.querySelector('#chosenDice').innerHTML;
 
-console.log(chosenDice);
+// to avoid the script to try loading the events if the inputs aren't on page
+
+if (rollButton != null) {
+    rollButton.addEventListener('click', rollClick); // On passe notre fonction en référence, c’est pourquoi c’est sans parenthèses
+};
+
+if(textButton != null) {
+    textButton.addEventListener('click', diceTextInput);
+}
 
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 };
 
-function rollClick() {
-    let dice = 1;
 
-    if (document.querySelector('#chosenDice').innerHTML === 'd10') {
-        dice = 10;
-    } else if (document.querySelector('#chosenDice').innerHTML === '2d10/4') {
-        dice = 25;
-    } else if (document.querySelector('#chosenDice').innerHTML === '2d10/2') {
-        dice = 50;
-    } else if (document.querySelector('#chosenDice').innerHTML === '2d10') {
-        dice = 100;
-    } else if (document.querySelector('#chosenDice').innerHTML === 'd20') {
-        dice = 20;
-    } else if (document.querySelector('#chosenDice').innerHTML === 'd12') {
-        dice = 12;
-    } else if (document.querySelector('#chosenDice').innerHTML === 'd8') {
-        dice = 8;
-    } else if (document.querySelector('#chosenDice').innerHTML === 'd6') {
-        dice = 6;
-    } else if (document.querySelector('#chosenDice').innerHTML === 'd4') {
-        dice = 4;
-    }
+// funtion to simullate a dice roll
+
+function rollClick() {
+    let dice = Number(tableDice.replace(/\D/g, ""));
 
     let result = getRandomInt(dice) + 1
-    console.log('Le résultat du lancé du dé est :' + result);
+    console.log('the dice rolled number is :' + result);
     choiceSelection(result);
 };
 
-function diceTextInput() {
+// funtion to input result of a player roll
 
+function diceTextInput() {
+    let textInput = document.getElementById('textRoll').value;
+    let result = textInput.replace(/\D/g, "");
+
+    let highLimit = Number(tableDice.replace(/\D/g, ""));
+
+    if(result > highLimit) {
+        console.log("the result input is higher than the table allows")
+        return "wrong input"
+    } else if (result < 1) {
+        console.log("the result input is lower than the table allows")
+        return "wrong input"
+    }
+    console.log('the dice rolled number is :' + result);
+    choiceSelection(result);
 };
 
+// function to showcase on the table which row as been roll by either text input or button click
+
 function choiceSelection(tableRow) {
-    if(document.querySelector(".selectedRow") === null){
-        console.log("if")
+    if (document.querySelector(".selectedRow") === null) {
         let selectedRow = document.querySelector('#ligne' + tableRow);
         selectedRow.className = "selectedRow";
     } else {
-        console.log("else")
-
         let previousRow = document.querySelector(".selectedRow");
         previousRow.classList.remove('selectedRow');
         let selectedRow = document.querySelector('#ligne' + tableRow);
         selectedRow.className = "selectedRow";
     }
-
 }
-
-if (rollButton != null) {
-    rollButton.addEventListener('click', rollClick); // On passe notre fonction en référence, c’est pourquoi c’est sans parenthèses
-};
